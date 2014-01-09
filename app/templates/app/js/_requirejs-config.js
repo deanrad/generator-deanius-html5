9deea649615ea3f6b56031984669cef0f497628f
Dean Radcliffe <dradcliffe@opinionlab.com>
2014-01-09 16:24:00 -0600

    Added text requirejs loader plugin

--- app/templates/app/js/_requirejs-config.js
+++ app/templates/app/js/_requirejs-config.js
@@ -10,11 +10,13 @@ var require={
     "jquery": "bower_components/jquery/jquery.min", /*data-alt-path="//code.jquery.com/jquery-1.10.2.min"*/
     "bootstrap": "bower_components/bootstrap/dist/js/bootstrap.min", /*data-alt-path="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min"*/
     "lodash": "bower_components/lodash/dist/lodash.min", /*data-alt-path="//cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min.js"*/
+    /* Loader plugins */
     "css": "bower_components/require-css/css", /*data-alt-path="//www.chicagogrooves.com/js/require-css.min.js"*/
     "coffee": "bower_components/require-cs/cs", /*data-alt-path="//cdnjs.cloudflare.com/ajax/libs/require-cs/0.4.2/cs.js"*/
     "coffee-script": "bower_components/coffee-script/index", /*data-alt-path="//cdnjs.cloudflare.com/ajax/libs/coffee-script/1.6.3/coffee-script.min.js"*/
     "domReady": "bower_components/requirejs-domready/domReady", /*data-alt-path="//cdnjs.cloudflare.com/ajax/libs/require-domReady/2.0.1/domReady.js"*/
-    "hbs": "bower_components/require-handlebars-plugin/hbs" /*data-alt-path="//cdnjs.cloudflare.com/ajax/libs/requirejs-handlebars/0.0.2/hbars.min.js"*/
+    "hbs": "bower_components/require-handlebars-plugin/hbs", /*data-alt-path="//cdnjs.cloudflare.com/ajax/libs/requirejs-handlebars/0.0.2/hbars.min.js"*/
+    "text": "bower_components/requirejs-text/text" /*data-alt-path="//www.chicagogrooves.com/js/requirejs-text.js"*/
   },
   shim: {
     "bootstrap": ["jquery"]
